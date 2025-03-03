39df04ef7652ac653d08fc594193a536e8968d87
sumin <sonsumin00@gmail.com>
2025-03-03 16:59:09 +0900

    #2 :sparkles: [Feat] : 예외 필터 적용

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,12 @@
-import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { NestFactory } from '@nestjs/core';
+import { HttpExceptionFilter } from './global/exception/filter/http-exception.filter';
 
-import { ClassSerializerInterceptor, HttpException, ValidationPipe } from '@nestjs/common';
-
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(CookieParser());
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      exceptionFactory: (errors) => {
-        const error = errors[0];
-        let message = 'Validation error occured';
-
-        if (error.constraints) {
-          message = Object.values(error.constraints)[0];
-        } else if (error.children && error.children.length > 0) {
-          const child = error.children[0];
-          if (child.constraints) {
-            message = Object.values(child.constraints)[0];
-          }
-        }
-        return new ValidationException([message]);
-      },
-    }),
-  );
-
   await app.listen(3000);
 }
 bootstrap();
