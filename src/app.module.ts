7384eb7aa0be453020f4d5934b638f2522917237
sumin <sonsumin00@gmail.com>
2025-03-03 13:54:07 +0900

    #1 :sparkles: [Feat] : jwt 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //전역 모듈로 설정(모든 곳에서 사용 가능)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+import { ClassSerializerInterceptor, HttpException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(CookieParser());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const error = errors[0];
+        let message = 'Validation error occured';
+
+        if (error.constraints) {
+          message = Object.values(error.constraints)[0];
+        } else if (error.children && error.children.length > 0) {
+          const child = error.children[0];
+          if (child.constraints) {
+            message = Object.values(child.constraints)[0];
+          }
+        }
+        return new ValidationException([message]);
+      },
+    }),
+  );
+
+  await app.listen(3000);
 }
 bootstrap();
