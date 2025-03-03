2d56de5b00f92edbd2e4584c6380dea43d6c98e0
sumin <sonsumin00@gmail.com>
2025-03-03 16:00:48 +0900

    #1 :sparkles: [Feat] : 인터셉터 설정

---
+++ src/global/common/interceptor/response.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { ResponseDto} from '../../exception/dto/response.dto';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((body) => {
+        const response = context.switchToHttp().getResponse();
+        if (body instanceof ResponseDto) {
+          response.status(body.httpStatus);
+        }
+        return body;
+      }),
+    );
+  }
+}
