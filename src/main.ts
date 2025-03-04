389f1c80938eb89d5c34e10c2eedd68a89cb4cc9
sumin <sonsumin00@gmail.com>
2025-03-04 15:05:31 +0900

    #1 :sparkles: [Feat] : 의존성 및 환경설정 추가

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { HttpExceptionFilter } from './global/exception/filter/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('털뭉치 API')
+    .setDescription('털뭉치 Documentation')
+    .setVersion('1.0')
+    .addBearerAuth() // JWT 토큰 인증 추가 (옵션)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // 기본 URL: http://localhost:3000/api
+
   await app.listen(3000);
 }
 bootstrap();
