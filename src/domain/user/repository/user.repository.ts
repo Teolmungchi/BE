1ecb7b542ff5f64c66c9557da45276cbe724f80f
sumin <sonsumin00@gmail.com>
2025-03-04 15:06:13 +0900

    #2 :recycle: [Refactor] : 파일 삭제 및 이름 변경

--- src/domain/user/repository/user.repository.ts
+++
@@ -1,6 +0,0 @@
-import { Repository } from 'typeorm';
-import { Injectable } from '@nestjs/common';
-import { User } from '../entity/user.entity';
-
-@Injectable()
-export class UserRepository extends Repository<User> {}
