d92e057ac1a161cf251be935bbd97de9de893b0f
sumin <sonsumin00@gmail.com>
2025-03-04 15:08:06 +0900

    #5 :sparkles: [Feat] : 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { LoggerMiddleware } from './global/common/middleware/logger/logger.middleware';
-import { UserModule } from './domain/user/module/user.module';
+import { UsersModule } from './domain/users/module/users.module';
 import { DatabaseModule } from './global/database/module/database.module';
+import { AuthModule } from './global/auth/module/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UsersModule, AuthModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
