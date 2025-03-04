7ca7d3643ee66c22dd729486c2c1013cea12b17e
sumin <sonsumin00@gmail.com>
2025-03-04 15:08:00 +0900

    #5 :sparkles: [Feat] : 인증/인가 API 구현

---
+++ src/global/auth/service/auth.service.ts
@@ -0,0 +1,111 @@
+import { Injectable, UseFilters } from '@nestjs/common';
+import { HttpExceptionFilter } from '../../exception/filter/http-exception.filter';
+import { JwtService } from '@nestjs/jwt';
+import { UserRepository } from '../../../domain/users/repository/user.repository';
+import { CommonException } from '../../exception/common-exception';
+import { ErrorCode } from '../../exception/error-code';
+import { AuthSignUpDto } from '../dto/auth-sign-up.dto';
+import { JwtTokenDto } from '../dto/jwt-token.dto';
+import { ChangePasswordDto } from '../dto/change-password.to';
+import * as bcrypt from 'bcryptjs';
+import { AuthLoginDto } from '../dto/auth-login.dto';
+
+@Injectable()
+@UseFilters(HttpExceptionFilter)
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async signUp(authSignUpDto: AuthSignUpDto): Promise<void> {
+    const hashedPassword = await bcrypt.hash(authSignUpDto.password, 10);
+    await this.userRepository.save({
+      serialId: authSignUpDto.userId,
+      password: hashedPassword,
+      name: authSignUpDto.name,
+    });
+  }
+
+  async login(authLoginDto: AuthLoginDto): Promise<JwtTokenDto> {
+    const user = await this.userRepository.findOne({
+      where: { serialId: authLoginDto.userId },
+    });
+
+    if (!user) {
+      throw new CommonException(ErrorCode.NOT_FOUND_USER);
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      authLoginDto.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new CommonException(ErrorCode.FAILURE_LOGIN);
+    }
+
+    const tokens = this.generateTokens(user.id);
+    user.refreshToken = tokens.refreshToken;
+    user.isLogin = true;
+    await this.userRepository.save(user);
+
+    return tokens;
+  }
+
+  async logout(userId: number): Promise<void> {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (user) {
+      user.refreshToken = null;
+      user.isLogin = false;
+      await this.userRepository.save(user);
+    }
+  }
+
+  async changePassword(
+    userId: number,
+    changePasswordDto: ChangePasswordDto,
+  ): Promise<void> {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new CommonException(ErrorCode.NOT_FOUND_USER);
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      changePasswordDto.currentPassword,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new CommonException(ErrorCode.FAILURE_CHANGE_PASSWORD);
+    }
+
+    const hashedPassword = await bcrypt.hash(changePasswordDto.newPassword, 10);
+    user.password = hashedPassword;
+    await this.userRepository.save(user);
+  }
+
+  async reissue(userId: number, refreshToken: string): Promise<JwtTokenDto> {
+    const user = await this.userRepository.findOne({
+      where: { id: userId, refreshToken, isLogin: true },
+    });
+
+    if (!user) {
+      throw new CommonException(ErrorCode.NOT_FOUND_LOGIN_USER);
+    }
+
+    const tokens = this.generateTokens(user.id);
+    user.refreshToken = tokens.refreshToken;
+    await this.userRepository.save(user);
+
+    return tokens;
+  }
+
+  private generateTokens(userId: number): JwtTokenDto {
+    const payload = { userId };
+    const accessToken = this.jwtService.sign(payload, { expiresIn: '1h' }); // accessToken 1시간 유효
+    const refreshToken = this.jwtService.sign(payload, { expiresIn: '14d' }); // refreshToken 14일 유효
+
+    return { accessToken, refreshToken };
+  }
+}
