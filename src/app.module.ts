3faec582c25c613236f216501dc61e648cc0cc1e
sumin <sonsumin00@gmail.com>
2025-03-06 17:51:33 +0900

    #4 :recycle: [Refactor] : 스크립트 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { LoggerMiddleware } from './global/common/middleware/logger/logger.middleware';
 import { UsersModule } from './domain/users/module/users.module';
+import * as dotenv from 'dotenv';
 import { DatabaseModule } from './global/database/module/database.module';
 import { AuthModule } from './global/auth/module/auth.module';
 
+dotenv.config();
 @Module({
   imports: [DatabaseModule, UsersModule, AuthModule],
 })
