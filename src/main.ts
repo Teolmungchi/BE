4632c960820adf616bcecf2906d21ebdd8f56a7d
sumin <sonsumin00@gmail.com>
2025-03-21 10:01:34 +0900

    #4 :recycle: [Refactor] : 스웨거 경로 표기 수정

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     .setDescription('털뭉치 Documentation')
     .setVersion('1.0')
     .addBearerAuth()
-    .setBasePath('api')
+    .addServer('/api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
