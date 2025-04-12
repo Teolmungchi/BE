d8b6b6b035eaf94c5db4efb26148668baa2ef91c
sumin <sonsumin00@gmail.com>
2025-04-12 22:15:10 +0900

    #4 :recycle: [Refactor] : s3 swagger response 설정

--- src/domain/s3/controller/minio.controller.ts
+++ src/domain/s3/controller/minio.controller.ts
@@ -26,7 +26,30 @@ export class MinioController {
   constructor(private readonly minioService: MinioService) {}
 
   @ApiOperation({ summary: 'S3 Presigned URL (업로드)' })
-  @ApiResponse({ status: 200, description: 'Presigned URL 반환 성공' })
+  @ApiResponse({
+    status: 200,
+    description: 'Presigned URL 반환 성공',
+    schema: {
+      example: {
+        httpStatus: 200,
+        success: true,
+        data: {
+          url: 'http://localhost:9000/tmc/1744354182542_27999?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=root%2F20250411%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250411T064942Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=96f13378a43c2f2130be1929e6fb783fd971d776a70c6efedb241f590c76cac1',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'JWT 토큰이 없거나 유효하지 않음',
+    schema: {
+      example: {
+        success: false,
+        message: 'Unauthorized',
+        statusCode: 401,
+      },
+    },
+  })
   @Get('upload')
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
@@ -37,6 +60,29 @@ export class MinioController {
   }
 
   @ApiOperation({ summary: 'S3 Presigned URL (다운로드)' })
+  @ApiResponse({
+    status: 400,
+    description: 'URL 생성 실패',
+    schema: {
+      example: {
+        success: false,
+        error: '파일 다운로드 URL 생성에 실패했습니다.',
+        statusCode: 500,
+        message: '파일 다운로드 URL 생성에 실패했습니다.',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'JWT 토큰이 없거나 유효하지 않음',
+    schema: {
+      example: {
+        success: false,
+        message: 'Unauthorized',
+        statusCode: 401,
+      },
+    },
+  })
   @Get('download/:fileName')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
