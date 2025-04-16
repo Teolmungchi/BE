1919f6c829730eda62272d4e999542ce5efafce5
sumin <sonsumin00@gmail.com>
2025-04-16 14:21:09 +0900

    #3 :recycle: [refactor] : 회원 정보 조회 시 아이디값 받아오게 수정

--- src/domain/users/dto/users.dto.ts
+++ src/domain/users/dto/users.dto.ts
@@ -2,11 +2,13 @@ import { User } from '../entity/user.entity';
 
 export class UsersDto {
   readonly id: number;
+  readonly login_id: string;
   readonly name: string;
 
   static fromEntity(user: User) {
     return {
       id: user.id,
+      login_id: user.serialId,
       name: user.name,
     };
   }
