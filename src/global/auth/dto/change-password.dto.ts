36add6a208ff644f0c0a7dd87bf52e8ca113dadf
sumin <sonsumin00@gmail.com>
2025-04-26 21:25:34 +0900

    #3 :recycle: [Refactor] : 비밀번호 변경 password dto 수정

--- src/global/auth/dto/change-password.dto.ts
+++ src/global/auth/dto/change-password.dto.ts
@@ -18,13 +18,13 @@ export class ChangePasswordDto {
 
   @ApiProperty({
     description: '현재 비밀번호',
-    example: '12345a!',
+    example: 'example123!@',
   })
   @IsNotEmpty({ message: '비밀번호는 null이 될 수 없습니다.' })
   @IsString()
-  @Matches(/^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{10,20}$/, {
+  @Matches(/^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,20}$/, {
     message:
-      '비밀번호는 소문자 1개 이상, 숫자 1개 이상, 특수문자(!, @, #, %, $) 1개 이상으로 구성된 10~20자리 비밀번호로 입력해주세요.',
+      '비밀번호는 소문자 1개 이상, 숫자 1개 이상, 특수문자(!, @, #, %, $) 1개 이상으로 구성된 8~20자리 비밀번호로 입력해주세요.',
   })
   @Expose({ name: 'newPassword' })
   newPassword: string;
