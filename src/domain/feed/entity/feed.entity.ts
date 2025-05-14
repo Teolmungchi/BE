6af21a58e2d9ce7d852e60193459a467c6de063d
sumin <sonsumin00@gmail.com>
2025-05-14 19:59:53 +0900

    #49 :sparkles: [Feat] : Feed entity

--- src/domain/feed/entity/feed.entity.ts
+++ src/domain/feed/entity/feed.entity.ts
@@ -1,19 +1,30 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from '../../users/entity/user.entity';
 import { BaseTime } from '../../../global/utils/basetime.entity';
+import { MatchingResult } from '../../match/entity/matching-result.entity';
 
 @Entity()
-export class Feed extends BaseTime{
+export class Feed extends BaseTime {
   @PrimaryGeneratedColumn({ name: 'feed_id' })
   id: number;
 
   @ManyToOne(() => User, (user) => user.id, { onDelete: 'CASCADE' })
   author: User;
 
-  @Column({ name: 'title', type: 'text', charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci' })
+  @Column({
+    name: 'title',
+    type: 'text',
+    charset: 'utf8mb4',
+    collation: 'utf8mb4_unicode_ci',
+  })
   title: string;
 
-  @Column({ name: 'content', type: 'text', charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci' })
+  @Column({
+    name: 'content',
+    type: 'text',
+    charset: 'utf8mb4',
+    collation: 'utf8mb4_unicode_ci',
+  })
   content: string;
 
   @Column({ name: 'file_name', type: 'varchar', length: 255 })
@@ -22,31 +33,69 @@ export class Feed extends BaseTime{
   @Column({ name: 'lost_date', type: 'date', nullable: true })
   lostDate: Date;
 
-  @Column({ name: 'lost_place', type: 'text', charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci', nullable: true })
+  @Column({
+    name: 'lost_place',
+    type: 'text',
+    charset: 'utf8mb4',
+    collation: 'utf8mb4_unicode_ci',
+    nullable: true,
+  })
   lostPlace: string;
 
-  @Column({ name: 'place_feature', type: 'text', charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci', nullable: true })
+  @Column({
+    name: 'place_feature',
+    type: 'text',
+    charset: 'utf8mb4',
+    collation: 'utf8mb4_unicode_ci',
+    nullable: true,
+  })
   placeFeature: string;
-
 
   /**
    * 강아지 특징
    */
-  @Column({ name: 'dog_type', type: 'text', charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci', nullable: true })
+  @Column({
+    name: 'dog_type',
+    type: 'text',
+    charset: 'utf8mb4',
+    collation: 'utf8mb4_unicode_ci',
+    nullable: true,
+  })
   dogType: string;
 
   @Column({ name: 'dog_age', type: 'int', nullable: true })
   dogAge: number;
 
-  @Column({ name: 'dog_gender', type: 'text', charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci', nullable: true })
+  @Column({
+    name: 'dog_gender',
+    type: 'text',
+    charset: 'utf8mb4',
+    collation: 'utf8mb4_unicode_ci',
+    nullable: true,
+  })
   dogGender: string;
 
-  @Column({ name: 'dog_color', type: 'text', charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci', nullable: true })
+  @Column({
+    name: 'dog_color',
+    type: 'text',
+    charset: 'utf8mb4',
+    collation: 'utf8mb4_unicode_ci',
+    nullable: true,
+  })
   dogColor: string;
 
-  @Column({ name: 'dog_feature', type: 'text', charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci', nullable: true })
+  @Column({
+    name: 'dog_feature',
+    type: 'text',
+    charset: 'utf8mb4',
+    collation: 'utf8mb4_unicode_ci',
+    nullable: true,
+  })
   dogFeature: string;
 
   @Column({ name: 'likes_count', type: 'int', default: 0 })
   likesCount: number;
+
+  @OneToMany(() => MatchingResult, (mr) => mr.feed)
+  matchingResults: MatchingResult[];
 }
