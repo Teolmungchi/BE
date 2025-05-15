80339ad700c13cba18cd829bc0c3122e8a9d054f
sumin <sonsumin00@gmail.com>
2025-05-15 14:29:10 +0900

    #49 :sparkles: [Feat] : 매칭 swagger 설정

--- src/domain/match/controller/match.controller.ts
+++ src/domain/match/controller/match.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { MatchService } from '../match.service';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../../../global/common/guards/jwt-auth.guard';
 import { ResponseDto } from '../../../global/exception/dto/response.dto';
 
@@ -12,7 +12,31 @@ export class MatchController {
   constructor(private readonly matchService: MatchService) {}
 
   @Post(':feedId')
-  @ApiOperation({ summary: '실종동물 매칭', description: '잃어버린 동물의 유사도를 기반으로 실종동물을 매칭합니다.' })
+  @ApiOperation({
+    summary: '실종동물 매칭',
+    description: '잃어버린 동물의 유사도를 기반으로 실종동물을 매칭합니다.',
+  })
+  @ApiBody({
+    schema: {
+      example: {
+        similarity: 80,
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: '매칭 결과 반환',
+    schema: {
+      example: {
+        httpStatus: 200,
+        success: true,
+        data: {
+          message: '잃어버린 동물을 제보해주셨어요! 유사도 80%!',
+          userId: 18,
+        },
+      },
+    },
+  })
   async match(
     @Req() req,
     @Param('feedId') feedId: number,
