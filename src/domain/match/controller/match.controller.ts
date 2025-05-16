52df2e48745f1ba80193ada4f49e013152a94f3d
sumin <sonsumin00@gmail.com>
2025-05-16 15:00:45 +0900

    #5 :recycle: [Refactor] : 매칭 알고리즘 수정

--- src/domain/match/controller/match.controller.ts
+++ src/domain/match/controller/match.controller.ts
@@ -11,6 +11,31 @@ import { ResponseDto } from '../../../global/exception/dto/response.dto';
 export class MatchController {
   constructor(private readonly matchService: MatchService) {}
 
+  @ApiOperation({
+    summary: '실종동물 매칭',
+    description: '잃어버린 동물의 유사도를 기반으로 실종동물을 매칭합니다.',
+  })
+  @ApiBody({
+    schema: {
+      example: {
+        similarity: 80,
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: '매칭 결과 반환',
+    schema: {
+      example: {
+        httpStatus: 200,
+        success: true,
+        data: {
+          message: '잃어버린 동물을 제보해주셨어요! 유사도 80%!',
+          userId: 18,
+        },
+      },
+    },
+  })
   @Post()
   async matchBulk(
     @Req() req,
